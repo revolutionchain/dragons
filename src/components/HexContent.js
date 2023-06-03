9256b08cb7b83852d582a157a4300869564ca0b0
cluz123 <pmlizarraga@gmail.com>
2023-06-03 02:29:09 -0300

    hexagons rotated 90º, hexagons displayed only if it is max zoom

--- src/components/HexContent.js
+++ src/components/HexContent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef  } from 'react';
-import { MapContainer, TileLayer, Polygon, Tooltip, Pane, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Polygon, Popup, useMap  } from 'react-leaflet';
 import {CRS} from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../styles/Home.css'
@@ -10,6 +10,29 @@ const Mapa = () => {
   const [minZoomState, setMinZoomState] = useState(3); 
   const [pageLoaded, setPageLoaded] = useState(false); 
   const [ selectedTile, setSelectedTile ] = useState(-90);
+  const [currentZoom, setCurrentZoom] = useState(3);
+
+  function MyMapComponent() {
+    const map = useMap();
+
+    useEffect(() => {
+      const handleZoomChange = () => {
+        const zoom = map.getZoom();
+        console.log('Zoom actual:', zoom);
+        setCurrentZoom(zoom);
+      };
+
+      map.on('zoomend', handleZoomChange);
+
+      return () => {
+        map.off('zoomend', handleZoomChange);
+      };
+    }, [map]);
+
+    return null;
+  }
+
+
 
   useEffect(() => {
     // Definir los límites del mapa
@@ -41,21 +64,21 @@ const Mapa = () => {
       let lng = swLng;
       let offset = 0;
       if (rowOffset % 2 !== 0) {
-        offset = cellSize ;
+        offset = cellSize+0.5 ;
       }
       while (lng < neLng) {
         const hexCoords = [
-          [lat, lng + offset],
-          [lat + cellSize / 2, lng + cellSize + offset],
-          [lat + cellSize + cellSize / 2, lng + cellSize + offset],
-          [lat + cellSize * 2, lng + offset],
-          [lat + cellSize + cellSize / 2, lng - cellSize + offset],
-          [lat + cellSize / 2, lng - cellSize + offset]
+          [lat, lng - (cellSize / 2) + offset],
+          [lat, lng + (cellSize /2) + offset],
+          [lat + (cellSize/2), lng + cellSize + offset],
+          [lat + cellSize, lng + (cellSize/2) + offset],
+          [lat +  cellSize, lng - (cellSize/2) + offset],
+          [lat + (cellSize/2), lng -cellSize + offset]
         ];
         hexGrid.push(hexCoords);
-        lng += cellSize * 2;
+        lng += cellSize * 3;
       }
-      lat += cellSize * 3 / 2;
+      lat += cellSize/2 ;
       rowOffset++;
     }
 
@@ -63,22 +86,32 @@ const Mapa = () => {
   };
 
 
+  /*
 
+          [lat, lng + offset],
+          [lat + cellSize / 2, lng + cellSize + offset],
+          [lat + cellSize + cellSize / 2, lng + cellSize + offset],
+          [lat + cellSize * 2, lng + offset],
+          [lat + cellSize + cellSize / 2, lng - cellSize + offset],
+          [lat + cellSize / 2, lng - cellSize + offset]
+
+*/
   return (
     <div>{ pageLoaded &&
     <MapContainer center={[0, 0]} zoom={3}
-    minZoom={minZoomState} // Nivel de zoom mínimo permitido
-    maxZoom={6} // Nivel de zoom máximo permitido
+    minZoom={minZoomState} // Nivel de zoom mínimo permitido pathOptions={selectedTile == index ? { fillColor: "yellow" } : { fillColor: "transparent" }}
+    maxZoom={6} // Nivel de zoom máximo permitido 
     crs={CRS.EPSG4326}
     maxBounds={bounds}
     maxBoundsViscosity={1} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
-        url={`https://${window.location.hostname}/qr/{z}/{x}/{y}.jpg`}
+        url={`http://${"localhost:3000"}/qr/{z}/{x}/{y}.jpg`}
         bounds={bounds}
       />
+      <MyMapComponent />
       {hexagonos.map((coords, index) => (     
         <Polygon key={index} positions={coords} color="yellow" fillColor={selectedTile == index ? "red" : "transparent"} weight={1}  
-        pathOptions={selectedTile == index ? { fillColor: "yellow" } : { fillColor: "transparent" }}
+        pathOptions={ {fillColor: selectedTile == index ? "yellow" : "transparent", color: currentZoom == 6 ? "yellow" : "transparent"} }
         eventHandlers={{
           click: () => setSelectedTile(index)
         }}
