067c8ece7300cebfbf2448fffd2f150fb06c20a9
cluz123 <pmlizarraga@gmail.com>
2023-06-16 01:02:27 -0300

    buttons hide functionality added

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -2,58 +2,63 @@ import React, { useEffect, useState } from 'react';
 import '../styles/Interface.css'
 import InventoryItem from './InventoryItem';
 
-export default function Inventory ()  {
+export default function Inventory() {
 
   const currentUrl = window.location.host == 'localhost:3000' ? 'http://localhost:3000' : `https://${window.location.hostname}`;
 
-    const inventory = [
-        { quantity: 100 },
-        { quantity: 100 },
-        { quantity: 100 },
-        {  quantity: 100 },
-        { quantity: 100 },
-        { quantity: 100 },
-        { quantity: 100 },
-        { quantity: 100 },
-        {  quantity: 100 },
-        { quantity: 100 },
-        { quantity: 100 },
-        { quantity: 100 },
-        {  quantity: 100 },
-        { quantity: 100 },
-        // Agrega más elementos al inventario si es necesario
-      ];
-    
-      const rows = [];
-      const itemsPerRow = 5;
-    
-      // Divide los elementos del inventario en filas
-      for (let i = 0; i < inventory.length; i += itemsPerRow) {
-        const rowItems = inventory.slice(i, i + itemsPerRow);
-        rows.push(rowItems);
-      }
+  const inventory = [
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    { quantity: 100 },
+    // Agrega más elementos al inventario si es necesario
+  ];
+
+  const rows = [];
+  const itemsPerRow = 5;
+
+  // Divide los elementos del inventario en filas
+  for (let i = 0; i < inventory.length; i += itemsPerRow) {
+    const rowItems = inventory.slice(i, i + itemsPerRow);
+    rows.push(rowItems);
+  }
+
+
+  const [buttonsState, setButtonsState] = useState(false);
   return (
     <div className='main-interface' >
-      <div className='inv-buttons-container'>
-      <button >1</button>
-      <div className='center-button-container'>
-        <button  >2</button>
-      </div>
-      <button >3</button>
-      </div>
-      <button style={{marginTop: "-10px"}}>X</button>
-        <div className='inv-img-container'>
-            <img className='inv-img' src={`${currentUrl}/images/inventory.jpg`} />
+      <div className={!buttonsState ? 'inv-buttons-container' : 'hidden'}>
+        <button>1</button>
+        <div className='center-button-container'>
+          <button>2</button>
         </div>
-    <div className="inv-content">
-      {rows.map((row, rowIndex) => (
-        <div className="inventory-row" key={rowIndex}>
-          {row.map((item, index) => (
-            <InventoryItem key={index} name={item.name} quantity={item.quantity} />
-          ))}
-        </div>
-      ))}
-    </div>
+        <button>3</button>
+      </div>
+      <button className={buttonsState ? 'hidden-button' : ''} style={{ marginTop: "-10px" }} onClick={() => {
+        setButtonsState(!buttonsState);
+      }}>{!buttonsState && 'X'}</button>
+      <div className='inv-img-container'>
+        <img className='inv-img' src={`${currentUrl}/images/inventory.jpg`} />
+      </div>
+      <div className="inv-content">
+        {rows.map((row, rowIndex) => (
+          <div className="inventory-row" key={rowIndex}>
+            {row.map((item, index) => (
+              <InventoryItem key={index} name={item.name} quantity={item.quantity} />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
